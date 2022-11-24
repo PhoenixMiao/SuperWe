e02e10446c00c557ed86646c0d63409b27d97235
24626 <2462660882@qq.com>
2022-11-24 15:42:06 +0800

    完成基础架构和基本功能

--- app/src/main/java/com/example/superwe/SuperApp.kt
+++ app/src/main/java/com/example/superwe/SuperApp.kt
@@ -1,6 +1,7 @@
 package com.example.superwe
 
 import android.app.Application
+import com.google.android.material.shape.ShapePath
 import com.orhanobut.hawk.Hawk
 import kotlin.properties.Delegates
 
@@ -13,5 +14,19 @@ class SuperApp : Application() {
         super.onCreate()
         instance = this
         Hawk.init(this).build()
+        Hawk.put(Constant.GROUP_NAME,"")
+        Hawk.put(Constant.FRIEND_LIST,"")
+        Hawk.put(Constant.GROUP_CHARGE,false)
+        Hawk.put(Constant.RECORD_ACTION,false)
+        Hawk.put(Constant.REPEAT_ACTION,false)
+        Hawk.put(Constant.WATCHER,arrayListOf<Pair<String?,String?>>())
+        Hawk.put(Constant.ADDING_FRIENDS_INTO_GROUP,false)
+        Hawk.put(Constant.AUTO_RECEIVE_LUCKY_MONEY,false)
+        Hawk.put(Constant.AUTO_ZAN,false)
+        Hawk.put(Constant.AUTO_REPLY,false)
+        Hawk.put(Constant.BATCH_REPLY,false)
+        Hawk.put(Constant.BATCH_REPLY_CONTENT,"")
+        Hawk.put(Constant.BATCH_READ,false)
+        Hawk.put(Constant.AUTO_REPLY_CONTENT,"")
     }
 }
